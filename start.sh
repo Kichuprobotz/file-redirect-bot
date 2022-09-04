e605871f132823f892780d55df468244be20a49a
Ckflicks <109861478+Kichuprobotz@users.noreply.github.com>
2022-09-04 19:49:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
+  git clone https://github.com/Kichuprobotz/file-redirect-bot.git /file-redirect-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TovinoBotV3
+  git clone $UPSTREAM_REPO /file-redirect-bot
 fi
 cd /TovinoBotV3
 pip3 install -U -r requirements.txt
-echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
+echo "Starting file-redirect-bot...."
 python3 bot.py
