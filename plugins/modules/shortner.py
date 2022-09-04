07506faac5c50d059435087d0c58a7348abe38be
Ckflicks <109861478+Kichuprobotz@users.noreply.github.com>
2022-09-04 19:46:22 +0530

    Update shortner.py

--- plugins/modules/shortner.py
+++ plugins/modules/shortner.py
@@ -15,7 +15,7 @@ OWLY_API = os.environ.get("OWLY_API", None)
 BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text='⚙ ᴅᴋ ʙᴏᴛx ⚙', url='https://t.me/ckflicks')
+            InlineKeyboardButton(text='⚙ckflicks⚙', url='https://t.me/ckflicks')
         ]
     ]
 )
